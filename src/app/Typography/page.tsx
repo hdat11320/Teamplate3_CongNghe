34fe0043e98477be2a45e8e692cb119d80b4e92f
Tran Văn Thanh <tranvanthanh060201@gmail.com>
2024-10-25 14:26:54 +0700

    first commit

---
+++ src/app/Typography/page.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import Typography from "@/components/Typography/Typography";
+
+
+export default function Home(){
+    return (
+        <>
+           <Header/>
+           <Typography></Typography>
+           <Footer/>
+        </>
+    )
+    
+}
